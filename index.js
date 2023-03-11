69fecb9f43cc3b196ccb72950c43036cb79642d4
Fnu Pawan Kumar <pawan.3940@gmail.com>
2023-03-11 03:30:05 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(name,eveName){
+    const newCard=[]
+    for(let i=0;i<name.length;i++) {
+       newCard[i]= `Thank you, ${name[i]}, for the wonderful ${eveName} gift!`;
+    }
+    return newCard;
+} 
+
+//console.log (writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+
+function countDown(intValue){
+    let i=0;
+    while (i <= intValue) {
+        console.log (i++);
+    }
+}
+//countDown(11)
